596289bd86f1c8b4cac5c2f1ff77ac964eebb960
Raphael Tang <raphpb1912@gmail.com>
2020-02-21 17:54:22 +0800

    Added detailed statistics 😋

--- lib/statistics.dart
+++ lib/statistics.dart
@@ -3,6 +3,9 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
+SizedBox smallSizedBox = SizedBox(height: 10);
+SizedBox bigSizedBox = SizedBox(height: 20);
+
 class Statistics extends StatefulWidget {
   @override
   _StatisticsState createState() => _StatisticsState();
@@ -10,6 +13,7 @@ class Statistics extends StatefulWidget {
 
 // TODO Add the expanding widget for details about the case
 // TODO Make the statistic number centered properly on pixel
+// TODO Make the statistic container width bigger
 class _StatisticsState extends State<Statistics> {
   String _totalConfirmed;
   String _totalDeaths;
@@ -20,8 +24,6 @@ class _StatisticsState extends State<Statistics> {
   String _totalClosed;
   String _totalClosedRecovered;
   String _totalClosedDeaths;
-
-  Widget _detailsWidget;
 
   @override
   void initState() {
@@ -64,7 +66,7 @@ class _StatisticsState extends State<Statistics> {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
               Container(
-                margin: EdgeInsets.only(top: 18, left: 20, bottom: 6),
+                margin: EdgeInsets.only(top: 18, left: 20, bottom: 10),
                 child: Text(
                   'Statistics',
                   style: TextStyle(
@@ -82,46 +84,131 @@ class _StatisticsState extends State<Statistics> {
                       data: _totalConfirmed,
                       textColor: Color(0xffFF6262),
                     ),
+                    bigSizedBox,
                     DataTile(
                       name: 'total deaths',
                       data: _totalDeaths,
                       textColor: Color(0xffB9B9B9),
                     ),
+                    bigSizedBox,
                     DataTile(
                       name: 'total recovered',
                       data: _totalRecovered,
                       textColor: Color(0xff71FFAE),
                     ),
-                    DataTile(
-                      name: 'active',
-                      data: _totalActive,
-                      textColor: Color(0xffFFD371),
-                      onTap: () {
-                        setState(() {
-                          if (_detailsWidget != null) {
-                            _detailsWidget = null;
-                          } else {
-                            _detailsWidget = Text(
-                              'test',
-                              style: TextStyle(color: Colors.white),
-                            );
-                          }
-                        });
-                      },
-                    ),
+                    bigSizedBox,
                     Container(
-                      child: _detailsWidget,
+                      child: Column(
+                        children: <Widget>[
+                          DataTile(
+                            name: 'active',
+                            data: _totalActive,
+                            textColor: Color(0xffFFD371),
+                          ),
+                          smallSizedBox,
+                          DataTileDetails(
+                            name: 'mild condition',
+                            data: _totalActiveMild,
+                            textColor: Color(0xffFFD371),
+                          ),
+                          smallSizedBox,
+                          DataTileDetails(
+                            name: 'serious condition',
+                            data: _totalActiveSerious,
+                            textColor: Color(0xffFFD371),
+                          ),
+                        ],
+                      ),
                     ),
-                    DataTile(
-                      name: 'closed',
-                      data: _totalClosed,
-                      textColor: Color(0xff71E5FF),
+                    bigSizedBox,
+                    Container(
+                      child: Column(
+                        children: <Widget>[
+                          DataTile(
+                            name: 'closed',
+                            data: _totalClosed,
+                            textColor: Color(0xff71E5FF),
+                          ),
+                          smallSizedBox,
+                          DataTileDetails(
+                            name: 'recovered',
+                            data: _totalClosedRecovered,
+                            textColor: Color(0xff71E5FF),
+                          ),
+                          smallSizedBox,
+                          DataTileDetails(
+                            name: 'deaths',
+                            data: _totalClosedDeaths,
+                            textColor: Color(0xff71E5FF),
+                          ),
+                        ],
+                      ),
                     ),
+                    SizedBox(height: 20),
                   ],
                 ),
               ),
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class DataTileDetails extends StatelessWidget {
+  const DataTileDetails({
+    @required String name,
+    @required String data,
+    @required Color textColor,
+  })  : _name = name,
+        _data = data,
+        _textColor = textColor;
+
+  final String _name;
+  final String _data;
+  final Color _textColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 18),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [
+            Color(0xff3B3B3B),
+            Color(0xff1C1C1C),
+          ],
+        ),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      height: MediaQuery.of(context).size.height * 0.09,
+      width: MediaQuery.of(context).size.width * 1,
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Text(
+              _name.toUpperCase(),
+              style: TextStyle(
+                letterSpacing: 5,
+                color: Color(0xffD0D0D0),
+                fontWeight: FontWeight.w700,
+              ),
+              textScaleFactor: 0.8,
+            ),
+            Text(
+              _data,
+              style: TextStyle(
+                color: _textColor,
+                fontWeight: FontWeight.w700,
+              ),
+              textScaleFactor: 2.5,
+            ),
+          ],
         ),
       ),
     );
@@ -147,52 +234,50 @@ class DataTile extends StatelessWidget {
     return GestureDetector(
       onTap: onTap,
       child: Container(
-        margin: EdgeInsets.symmetric(vertical: 10, horizontal: 18),
+        margin: EdgeInsets.symmetric(horizontal: 18),
         child: Container(
-          child: Container(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                colors: [
-                  Color(0xff3B3B3B),
-                  Color(0xff1C1C1C),
-                ],
-              ),
-              borderRadius: BorderRadius.circular(10),
-            ),
-            height: MediaQuery.of(context).size.height * 0.25,
-            width: MediaQuery.of(context).size.width * 1,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Container(
-                  margin: EdgeInsets.only(top: 16, left: 16),
-                  child: Text(
-                    name.toUpperCase(),
-                    style: TextStyle(
-                      letterSpacing: 5,
-                      color: Color(0xffD0D0D0),
-                      fontWeight: FontWeight.w700,
-                    ),
-                    textScaleFactor: 0.8,
-                  ),
-                ),
-                Center(
-                  child: Container(
-                    margin: EdgeInsets.all(36),
-                    child: Text(
-                      data ??= '',
-                      style: TextStyle(
-                        color: textColor,
-                        fontWeight: FontWeight.w700,
-                      ),
-                      textScaleFactor: 3.5,
-                    ),
-                  ),
-                ),
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Color(0xff3B3B3B),
+                Color(0xff1C1C1C),
               ],
             ),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          height: MediaQuery.of(context).size.height * 0.25,
+          width: MediaQuery.of(context).size.width * 1,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.only(top: 16, left: 16),
+                child: Text(
+                  name.toUpperCase(),
+                  style: TextStyle(
+                    letterSpacing: 5,
+                    color: Color(0xffD0D0D0),
+                    fontWeight: FontWeight.w700,
+                  ),
+                  textScaleFactor: 0.8,
+                ),
+              ),
+              Center(
+                child: Container(
+                  margin: EdgeInsets.all(36),
+                  child: Text(
+                    data ??= '',
+                    style: TextStyle(
+                      color: textColor,
+                      fontWeight: FontWeight.w700,
+                    ),
+                    textScaleFactor: 3.5,
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
