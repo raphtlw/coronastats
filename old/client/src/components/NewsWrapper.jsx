92156e82aa6b8c1f822109dd95e497c1a113022b
Raphael Tang <raphpb1912@gmail.com>
2020-05-21 10:12:14 +0800

    Removed old

--- old/client/src/components/NewsWrapper.jsx
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react';
-
-export default class NewsWrapper extends Component {
-  render() {
-    return <div className='NewsWrapper'>{this.props.children}</div>;
-  }
-}
--- old/client/src/components/Spacing.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-export default class Spacing extends Component {
-  render() {
-    return (
-      <div className='Spacing' style={{ height: this.props.height }}></div>
-    );
-  }
-}
